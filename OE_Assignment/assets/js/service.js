f65a77f67d4cfad693551d6ceff8d65902ba0246
rahulshivsharan <rahulshivsharan@gmail.com>
2017-08-06 01:01:11 +0530

    Completed create user module

--- OE_Assignment/assets/js/service.js
+++ OE_Assignment/assets/js/service.js
@@ -6,8 +6,12 @@
 	
 	function myService($http,$q){
 		var obj = {};
+		
 		obj.getItems = getItems;	
-		obj.getCountryByCodes = getCountryByCodes;	
+		obj.getCountryByCodes = getCountryByCodes;
+		obj.createUser = createUser;
+		obj.getUsers = getUsers;
+
 		return obj;
 
 		function getItems(){
@@ -39,6 +43,45 @@
 			function error(response){
 				deferred.reject(response);
 			}
-		} // end of getCountryByCodes		
+		} // end of getCountryByCodes
+
+		function createUser(firstName,lastName,sex,dob,country){
+			var url = "/users";
+			var deferred = $q.defer();
+			
+			$http.post(url,{
+				"firstName" : firstName,
+			  	"lastName" : lastName,
+			    "sex" : sex,
+			    "dob" : dob,
+			    "country" : country
+			}).then(success,error);
+
+			return deferred.promise;
+
+			function success(respose){
+				deferred.resolve(respose);
+			}
+
+			function error(response){
+				deferred.reject(response);
+			}
+		}// end of createUser	
+
+		function getUsers(){
+			var url = "/users";
+			var deferred = $q.defer();
+			$http.get(url).then(success,error);
+			
+			return deferred.promise;
+
+			function success(respose){
+				deferred.resolve(respose);
+			}
+
+			function error(response){
+				deferred.reject(response);
+			}
+		}// end of getUsers	
 	} // end of myService
 })();
--- OE_Assignment/assets/js/user.controller.js
+++ OE_Assignment/assets/js/user.controller.js
@@ -15,6 +15,7 @@
 		vm.country = "";
 		vm.dob = "";
 		vm.countryList = [];
+		vm.userList = [];
 		
 		vm.dateOptions = {
 			changeYear: true,
@@ -36,19 +37,44 @@
 		// public methods
 		vm.init = init;
 		vm.create = create;
+		vm.loadUsers = loadUsers;
 
 		function init(){
+			loadUsers();
 			loadCountries();			
 		} // end of init
 
 		function create(){
-			console.log("First Name ",vm.firstName);
-			console.log("Last Name ",vm.lastName);
-			console.log("Sex ",vm.sex);
+
+			// console.log("First Name ",vm.firstName);
+			// console.log("Last Name ",vm.lastName);
+			// console.log("Sex ",vm.sex);
 			var dateString = moment(vm.dob).format("YYYY-MM-DD");
-			console.log("Date of Birth ",dateString);
-			console.log("Country ",vm.country);
+			// console.log("Date of Birth ",dateString);
+			// console.log("Country ",vm.country);
+			myService.createUser(vm.firstName,vm.lastName,vm.sex,dateString,vm.country).then(success,error);
+
+			function success(response){
+				loadUsers();
+			} // end of success
+
+			function error(response){
+				console.log(response);
+			} // end of error
 		}// end of create
+
+		function loadUsers(){
+			myService.getUsers().then(success,error);
+
+			function success(response){
+				//console.log(response["data"]);
+				vm.userList = response["data"];
+			} // end of success
+
+			function error(response){
+				console.log(response);
+			} // end of error
+		} // end of loadUsers
 
 		function loadCountries(){
 			myService.getCountryByCodes().then(success,error);
